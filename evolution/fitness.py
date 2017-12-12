7114afe658bcb6157d66d2686f57d4e8d6e0f937
Felipe Menelau <felipe.menelau@inlocomedia.com>
2017-12-11 21:54:02 -0300

    Adds fitness function

---
+++ evolution/fitness.py
@@ -0,0 +1,43 @@
+import pandas as pd
+# Globals #
+# Idealmente em variaveis de ambient/vindos do db
+PESO_GERENCIAL = 1
+PESO_CARGA_HORARIA = 1
+PESO_CONHECIMENTO = 1
+# End Globals #
+
+def fitness(funcao):
+    servidores_alocados = funcao.servidores_alocados
+    results = pd.DataFrame(columns = ['servidor', 'funcao', 'fitness'])
+
+    for servidor in servidores_alocados:
+        parametro_gerencial = calcular_parametro_gerencial(servidor, funcao)
+        parametro_de_carga_horaria = calcular_parametro_de_carga horaria(servidor, funcao)
+        parametro_de_conhecimento = calcular_parametro_de_conhecimento(servidor, funcao)
+
+        denominador_fitness = (parametro_gerencial*PESO_GERENCIAL +
+                                parametro_de_carga_horaria*PESO_CARGA_HORARIA +
+                                parametro_de_conhecimento*PESO_CONHECIMENTO)
+
+        numerador_fitness = PESO_GERENCIAL + PESO_CARGA_HORARIA + PESO_CONHECIMENTO
+        fitness = denominador_fitness/numerador_fitness
+        results = results.append({'servidor': servidor.matricula, 'funcao': funcao.chave,
+                                    'fitness': fitness}, ignore_index=True)
+    return results
+
+def calcular_parametro_gerencial(servidor, funcao):
+    if servidor.experiencia_em_gestao() == funcao.pratica_gerencial:
+        return 1
+    else:
+        return 0
+
+#TODO
+def calcular_parametro_de_carga_horaria(servidor, funcao):
+    return 1
+
+def calcular_parametro_de_conhecimento(servidor, funcao):
+    match_servidor = 0
+    for conhecimento, peso in servidor.areas_de_conhecimento:
+        if conhecimento in funcao.areas_de_conhecimento:
+            match_servidor += 1
+    return float(match_servidor)/float(len(funcao.areas_de_conhecimento.keys()))
