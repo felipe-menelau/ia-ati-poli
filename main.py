2132de98436e59cb1b4b4bfb5e97a3bd261b1201
Felipe Menelau <felipe.menelau@inlocomedia.com>
2017-12-17 14:52:40 -0300

    Fixes formatting and now accepts parameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from random import randint
+from syste import argv
 from evolution.fitness import fitness
 from evolution.initial_population import gerar_individuo
 from evolution.mutation import mutation
@@ -7,30 +8,31 @@ from evolution.reproduction import regularReproduction
 from parser.parser_funcao import read_file as read_funcao
 from parser.parser_servidor import read_file as read_servidor
 
+
 def main():
-        # read_file_funcao
-        funcoes = read_funcao("file_function")
-        valor_aceitavel = 0.8
-        servidores = read_servidor("file")
-        fitness_populacao = 0
-        fitness = 0
-        populacao = gerar_individuo(servidores, funcoes)
-        random_function_1 = randint()
-        random_function_2 = randint(0, random_function_1)
-        populacao_reproduzida = regularReproduction(individuos[random_function_1], individuos[random_function_2])
+    funcoes_filename = argv[1]
+    servidores_filename = arvg[2]
 
-        while fitness_populacao < valor_aceitavel || fitness < valor_aceitavel:
+    valor_aceitavel = 0.8
+    fitness_populacao = 0
+    fitness = 0
 
-            for individuo in populacao:
-                obj = fitness(individuo)
-                fitness += obj['fitness']
+    funcoes = read_funcao(funcoes_filename)
+    servidores = read_servidor(servidores_filename)
 
-                pass
-            for individuo_reproduzido in populacao_reproduzida:
-                obj = fitness(individuo_reproduzido)
-                fitness_populacao += obj['fitness']
+    populacao = gerar_individuo(servidores, funcoes)
+    random_function_1 = randint()
+    random_function_2 = randint(0, random_function_1)
+    populacao_reproduzida = regularReproduction(individuos[random_function_1], individuos[random_function_2])
 
-                pass
+    while fitness_populacao < valor_aceitavel || fitness < valor_aceitavel:
 
-        pass
-pass
+        for individuo in populacao:
+            obj = fitness(individuo)
+            fitness += obj['fitness']
+
+        for individuo_reproduzido in populacao_reproduzida:
+            obj = fitness(individuo_reproduzido)
+            fitness_populacao += obj['fitness']
+
+        
