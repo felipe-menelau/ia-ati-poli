4265886bc1e49eedcfccd728aa296783d8f2860d
Vinicius Barros <vinicius0728@gmail.com>
2017-12-18 20:50:27 -0300

    Adjust mutation conflicts #2

--- evolution/mutation.py
+++ evolution/mutation.py
@@ -18,6 +18,7 @@ def mutation(some_allocation):
 
         random_server_1 = randint(0, len(first_servers)-1)
         random_server_2 = randint(0, len(second_servers)-1)
+
         if str(second_function.funcao) != 'nan':
             if (((first_servers[random_server_1]. cargo in second_function.funcao) or
                 (first_function[random_server_1].funcao in second_function.funcao) and
@@ -27,7 +28,8 @@ def mutation(some_allocation):
                  first_servers[random_server_1].experiencia_em_gestao() == second_function.pratica_gerencial and
                  second_function.horas_slotadas() == first_function.horas_slotadas() or
                  second_function.horas_slotadas <= first_function.horas_slotadas + first_function.forca_de_trabalho_s or
-                 first_function.horas_slotadas <= second_function.horas_slotadas + second_function.forca_de_trabalho_s):
+                 first_function.horas_slotadas <= second_function.horas_slotadas + second_function.forca_de_trabalho_s and
+                 first_function.servidores_alocados != [] and second_function.servidores_alocados != [] ):
 
                 temporary_holder = first_servers[random_server_1]
 
