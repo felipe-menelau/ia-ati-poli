539fab679843a226229f0d0b8fc0eaae03ae4931
Vinicius Barros <vinicius0728@gmail.com>
2017-12-18 20:53:33 -0300

    Adjust mutation conflicts #3

--- evolution/mutation.py
+++ evolution/mutation.py
@@ -13,35 +13,36 @@ def mutation(some_allocation):
         first_function = some_allocation[random_function_1]
         second_function = some_allocation[random_function_2]
 
-        first_servers = first_function.servidores_alocados
-        second_servers = second_function.servidores_alocados
+        if first_function.servidores_alocados and second_function.servidores_alocados:
+            first_servers = first_function.servidores_alocados
+            second_servers = second_function.servidores_alocados
 
-        random_server_1 = randint(0, len(first_servers)-1)
-        random_server_2 = randint(0, len(second_servers)-1)
+            random_server_1 = randint(0, len(first_servers)-1)
+            random_server_2 = randint(0, len(second_servers)-1)
 
-        if str(second_function.funcao) != 'nan':
-            if (((first_servers[random_server_1]. cargo in second_function.funcao) or
-                (first_function[random_server_1].funcao in second_function.funcao) and
-                (second_function[random_server_2].cargo in first_function.funcaoo) or
-                (second_function[random_server_2.funcao] in first_function.funcao)) and
-                 first_servers[random_server_1].experiencia_em_gestao() or
-                 first_servers[random_server_1].experiencia_em_gestao() == second_function.pratica_gerencial and
-                 second_function.horas_slotadas() == first_function.horas_slotadas() or
-                 second_function.horas_slotadas <= first_function.horas_slotadas + first_function.forca_de_trabalho_s or
-                 first_function.horas_slotadas <= second_function.horas_slotadas + second_function.forca_de_trabalho_s and
-                 first_function.servidores_alocados != [] and second_function.servidores_alocados != [] ):
+            if str(second_function.funcao) != 'nan':
+                if (((first_servers[random_server_1]. cargo in second_function.funcao) or
+                    (first_function[random_server_1].funcao in second_function.funcao) and
+                    (second_function[random_server_2].cargo in first_function.funcaoo) or
+                    (second_function[random_server_2.funcao] in first_function.funcao)) and
+                     first_servers[random_server_1].experiencia_em_gestao() or
+                     first_servers[random_server_1].experiencia_em_gestao() == second_function.pratica_gerencial and
+                     second_function.horas_slotadas() == first_function.horas_slotadas() or
+                     second_function.horas_slotadas <= first_function.horas_slotadas + first_function.forca_de_trabalho_s or
+                     first_function.horas_slotadas <= second_function.horas_slotadas + second_function.forca_de_trabalho_s and
+                     first_function.servidores_alocados != [] and second_function.servidores_alocados != [] ):
 
-                temporary_holder = first_servers[random_server_1]
+                    temporary_holder = first_servers[random_server_1]
 
-                first_servers[random_server_1] = second_servers[random_server_2]
-                second_servers[random_server_2] = temporary_holder
+                    first_servers[random_server_1] = second_servers[random_server_2]
+                    second_servers[random_server_2] = temporary_holder
 
-                first_function.servidores_alocados = first_servers
-                second_function.servidores_alocados = second_servers
+                    first_function.servidores_alocados = first_servers
+                    second_function.servidores_alocados = second_servers
 
-                some_allocation[random_function_1] = first_function
-                some_allocation[random_function_2] = second_function
+                    some_allocation[random_function_1] = first_function
+                    some_allocation[random_function_2] = second_function
 
-        number_of_mutations -=1
+            number_of_mutations -=1
 
     return some_allocation
